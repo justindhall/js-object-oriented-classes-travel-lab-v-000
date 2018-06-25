5b7e6ec7a3ff285b909c07aec8738a3f43d1a02c
Justin Hall <hall.justin.d@gmail.com>
2018-06-25 19:24:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,7 @@ class Route{
   }
   
   blocksTravelled(){
-    let eastWest = []
+    let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+    
   }
 }
