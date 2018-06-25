4fd2d65afc862a61be98b3d733e6b42c3a3e082e
Justin Hall <hall.justin.d@gmail.com>
2018-06-25 19:35:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
 class Driver{
   constructor(name, startDate){
     this.name = name;
@@ -16,8 +18,6 @@ class Route{
     this.beginningLocation = beginningLocation;
     this.endingLocation = endingLocation;
   }
-  
-  let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
   
   findAvenueIndex(avenue){
     return eastWest.indexOf(avenue);
