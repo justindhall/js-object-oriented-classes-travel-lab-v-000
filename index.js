111999ffee54c5dcc789ee61f8e313a7e2ec87f1
Justin Hall <hall.justin.d@gmail.com>
2018-06-25 19:29:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,9 +17,7 @@ class Route{
     this.endingLocation = endingLocation;
   }
   
-  AvenueinInt(avenue){
-    return 
-  }
+
   
   blocksTravelled(){
     let eastWest = ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
