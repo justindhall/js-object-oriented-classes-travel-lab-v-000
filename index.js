061a616d037e0cb6422d71105829d139b1b2df3f
Justin Hall <hall.justin.d@gmail.com>
2018-06-25 19:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
+class Driver{
+  constructor(name, startDate){
+    this.name = name;
+    this.startDate = new Date(startDate);
+  }
+  
+  yearsExperienceFromBeginningOf(year){
+    let endDate = new Date(year, 1, 1)
+    let totalExperience = (endDate - this.startDate)/(365*24*60*60*1000);
+    return Math.ceil(totalExperience);
+  }
+}
 
+class Route{
+  constructor(beginningLocation, endingLocation){
+    this.beginningLocation = beginningLocation;
+    this.endingLocation = endingLocation;
+  }
+  
+  blocksTravelled 
+}
